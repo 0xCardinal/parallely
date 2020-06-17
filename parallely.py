97e54a0a8e032c5cc367672a220451d98a6358b7
Kumar Ashwin <krashwin00@gmail.com>
2020-06-17 16:04:39 +0530

    Add files via upload

---
+++ parallely.py
@@ -0,0 +1,37 @@
+import time, threading, subprocess, sys
+from queue import Queue
+
+lock = threading.Lock()
+q = Queue()
+count = 0
+
+filename = sys.argv[1]
+url_list = []
+with open(filename,'r') as file:
+    url_list = file.read().split("\n")
+
+for worker in url_list:
+    q.put(worker)
+
+def download(worker):
+    check = subprocess.run(["wget",worker],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    if check != 0 :
+        count+=1
+        with lock:
+            print("Downloaded file from url: ",worker)
+
+def threader():
+    while True:
+        worker = q.get()
+        download(worker)
+        q.task_done()
+
+for x in range(10):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+start = time.time()
+q.join()
+
+print("Total time taken to download files: ", time.time()-start)
